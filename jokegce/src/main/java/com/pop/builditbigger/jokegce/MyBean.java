00383325bdb8922f748cc29aa7be895eccfc2d38
poph2 <niyipopp@yahoo.co.uk>
2016-09-13 01:00:06 +0100

    Created the GCE Library

---
+++ jokegce/src/main/java/com/pop/builditbigger/jokegce/MyBean.java
@@ -0,0 +1,15 @@
+package com.pop.builditbigger.jokegce;
+
+/** The object model for the data we are sending through endpoints */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
---
+++ jokegce/src/main/java/com/pop/builditbigger/jokegce/MyEndpoint.java
@@ -0,0 +1,44 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.pop.builditbigger.jokegce;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.pop.joker.Joker;
+
+import javax.inject.Named;
+
+/** An endpoint class we are exposing */
+@Api(
+  name = "myApi",
+  version = "v1",
+  namespace = @ApiNamespace(
+    ownerDomain = "jokegce.builditbigger.pop.com",
+    ownerName = "jokegce.builditbigger.pop.com",
+    packagePath=""
+  )
+)
+public class MyEndpoint {
+
+    /** A simple endpoint method that takes a name and says Hi back */
+    @ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+        response.setData("Hi, " + name);
+
+        return response;
+    }
+
+    public MyBean getJoke() {
+        MyBean response = new MyBean();
+        response.setData(Joker.getJoke());
+
+        return response;
+    }
+
+}
